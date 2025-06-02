ed451620ae85b3e31508d0a1b2a818c123d8a348
evandro dos santos dias <evandro.dias@cicc.local>
2025-06-02 13:51:04 -0300

    add-pasta-aula-05

--- recursao.js
+++ recursao.js
@@ -1,9 +1,10 @@
 contagem_regressiva = (n) => {
     console.log(n)
-    if (n === 0) {
+    
+    if (n === 0) { //caso base
         return
     }
-    // chamdada recursiva
-    contagem_regressiva(n - 1)
+    
+    contagem_regressiva(n - 1) // chamdada recursiva
 }
 contagem_regressiva(5)
